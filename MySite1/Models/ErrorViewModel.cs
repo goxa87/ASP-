5c5f351fab2cb98bed95306a252a1ac0685936a6
GeorgyPetrov <goxa87@rambler.ru>
2020-02-12 10:46:09 +0300

    Добавьте файлы проекта.

---
+++ MySite1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MySite1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
