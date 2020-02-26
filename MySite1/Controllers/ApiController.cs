13c351c0e4acde7ff5a6402b90282ca2941cbb6b
GeorgyPetrov <goxa87@rambler.ru>
2020-02-26 14:19:05 +0300

    2602 - api
    прибавление лайков , получение списка сообщений с сайта

---
+++ MySite1/Controllers/ApiController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MySite1.Data;
+using MySite1.Models;
+
+namespace MySite1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+
+    public class ApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ApiController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        [Route("worklike/{workId}")]
+        public async Task<ActionResult> WorkLike(int workId)
+        {
+            var work = await _db.Works.FirstOrDefaultAsync(e => e.WorkId == workId);
+
+            if (work == null)
+                return NotFound();
+            work.Likes++;
+            _db.Update(work);
+            await _db.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("postlike/{postId}")]
+        public async Task<ActionResult> PostLike(int postId)
+        {
+            var post = await _db.BlogPosts.FirstOrDefaultAsync(e => e.BlogPostId == postId);
+
+            if (post == null)
+                return NotFound();
+            post.Likes++;
+            _db.Update(post);
+            await _db.SaveChangesAsync();
+            return Ok();
+        }
+        /// <summary>
+        /// Возвращает сообщения на платформе
+        /// </summary>
+        /// <param name="password">пароль</param>
+        /// <param name="notreaden">только непрочитанные (1 - непрочитанные 0 - все)</param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("messages/pass/{password}/notreaden/{notreaden}")]
+        public async Task<List<ContactMessage>> GetMessages(string password, bool notreaden)
+        {
+            //List<ContactMessage> rezult = new List<ContactMessage>();
+
+            if (password != "qwerty123") return new List<ContactMessage> { new ContactMessage { From = "неверный пароль" } };
+
+            var select = await _db.ContactMessages.OrderByDescending(e=>e.Date).ToListAsync();
+            if (notreaden)
+            {
+                return select.Where(e => e.Readen == false).ToList();
+
+            }
+            else return select;
+        }
+
+    }
+}
